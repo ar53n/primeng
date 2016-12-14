13e543685f63705cd5e998ac2f76ac27a288edb1
ar53n <Arskarap@gmail.com>
2016-12-14 12:29:27 +0300

    Update webpack.prod.js

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -4,6 +4,7 @@ var commonConfig = require('./webpack.common.js');
 
 const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
 
+
 module.exports = webpackMerge(commonConfig, {
     plugins: [
         new webpack.NoErrorsPlugin(),
